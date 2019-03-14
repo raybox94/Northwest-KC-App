2aef1f8b761d78a9714c47a05801bf3e2c716c10
rayaan ahmed <rayaan@nw44083.nwmissouri.edu>
2019-03-14 09:42:37 -0500

    Initial Commit

---
+++ Northwest-KC-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Northwest-KC-App
+//
+//  Created by rayaan on 14/03/19.
+//  Copyright © 2019 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
